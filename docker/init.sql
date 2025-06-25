d18a08b032470d84ec8c1f9d98f64d45c8a7966f
Francisco Ricardo <franciscoricardo.dev@gmail.com>
2025-06-25 13:45:58 -0300

    First commit
     Changes to be committed:
    	new file:   .devcontainer/devcontainer.json
    	new file:   .gitignore
    	new file:   docker-compose-dev.yml
    	new file:   docker/Dockerfile.dev
    	new file:   docker/init.sql

---
+++ docker/init.sql
@@ -0,0 +1,6 @@
+-- Habilita a extensão PostGIS para suporte a dados geoespaciais
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+-- Habilita a extensão para tipos de geometria adicionais
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
+
